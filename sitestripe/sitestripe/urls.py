ecb48f9f279d5850caabfc9c1d6cd7681f3cb6f6
petr <petr@ubmate4.myguest.virtualbox.org>
2023-02-23 00:04:54 +0300

    urls corrected

--- sitestripe/sitestripe/urls.py
+++ sitestripe/sitestripe/urls.py
@@ -14,9 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from products.views import *
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("item/<int:item_id>/", item)
+    path("products/", include("products.urls")),
 ]
